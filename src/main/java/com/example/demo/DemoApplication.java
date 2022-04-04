ad82bacd1579f12ee2afd87f6e0acc66d1559d80
luo0412 <1095847440@qq.com>
2022-04-05 03:36:16 +0800

    adjust

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		var h = """
-				select * from hello
-				""";
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,11 +1,29 @@
 package com.example.demo.mapper;
 
+import com.example.demo.entity.User;
+import org.beetl.sql.mapper.BaseMapper;
+import org.beetl.sql.mapper.annotation.Sql;
+
 import java.util.List;
 
-import com.example.demo.entity.User;
-
-import org.beetl.sql.mapper.BaseMapper;
-
 public interface UserMapper extends BaseMapper<User> {
+    @Sql("""
+        select * 
+        from user 
+        where age = #{age}
+        order by id    
+    """)
     List<User> listByAge(Integer age);
+
+    @Sql("""
+        -- 我是注释
+        select *
+        from sys_user u
+        where 1=1
+        -- @if(!isBlank(userName)){
+            and user_name = #{userName}
+        -- @}          
+        order by u.dept_id desc
+    """)
+    List<User> listByUserName(String name);
 }
