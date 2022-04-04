4758720c0337f1b249f397867977ef2fc770f441
luo0412 <1095847440@qq.com>
2022-04-05 04:11:27 +0800

    adjust

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -2,19 +2,25 @@ package com.example.demo.entity;
 
 import java.util.Date;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import org.beetl.sql.annotation.entity.AutoID;
 import org.beetl.sql.annotation.entity.Table;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @Table(name="user")
 public class User {
     @AutoID
     private Long id;
+
     private String name;
     private Integer age;
 
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
     private Date createTime;
+
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 }
